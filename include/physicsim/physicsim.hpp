7c4e3edc17d34640437907d0c5d8c5965fd1c03c
hirok2005 <hirok2005@gmail.com>
2024-02-04 18:56:27 +0000

    set up cmake

---
+++ include/physicsim/physicsim.hpp
@@ -0,0 +1,12 @@
+﻿// physicsim.h
+
+#ifndef PHYSICSIM_H
+#define PHYSICSIM_H
+
+namespace physicsim {
+
+    void foo();
+
+} // namespace physicsim
+
+#endif // PHYSICSIM_H
---
+++ src/physicsim.cpp
@@ -0,0 +1,14 @@
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+
+namespace physicsim {
+
+		void foo()
+		{
+			sf::Window window(
+				sf::VideoMode(640, 480),
+				"Hello world");
+			while (true) {}
+		}
+}
